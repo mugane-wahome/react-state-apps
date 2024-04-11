22146a0b64b8212c78bed25819f1dc67f11a740d
mugane-wahome <muganewahome@gmail.com>
2024-04-11 09:58:31 +0300

    updated states

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,25 +4,25 @@ import "./meme.css"
 
 export default function Meme() {
    
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
-    
-    
-    function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }))
-        
-    }
-    
+ const [meme, setMeme]= useState({
+topText:"",
+bottomText:"",
+randomImage: "http://i.imgflip.com/1bij.jpg" 
+
+ })
+
+ const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+ function getMemeImage(){
+    const memesArray = allMemeImages.data.memes
+    const randomIndex = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomIndex].url
+    setMeme(prevMeme =>({
+        ...prevMeme,
+        randomImage: url
+    }))
+ }
+
     return (
         <main>
             <div className="form">
