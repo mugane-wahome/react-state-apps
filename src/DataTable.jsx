173d45fa7d15de5663da506e91b7e7fe43759ec0
mugane-wahome <muganewahome@gmail.com>
2024-06-03 11:16:46 +0300

    initial commit

---
+++ src/DataTable.jsx
@@ -0,0 +1,46 @@
+import React,{ useState } from "react"  ;
+import{
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Button,
+    Textfield,
+    IconButton
+} from '@mui/material';
+import {Edit,Delete} from '@mui/icons-material';
+
+const DataTable =()=>{
+
+    const [data, setData]=useState([
+        { id: 1, name: 'John Doe', gender: 'Male', age: 28 },
+        { id: 2, name: 'Jane Smith', gender: 'Female', age: 34 }
+    ]);
+
+    const [isEditing, setIsEditing] = useState(null);
+    const [formData, setFormData] = useState({ id: null, name: '', gender: '', age: '' });
+
+const handleAdd =()=>{
+    setData([...data, {...formData, id:data.length+1}]);
+    setFormData({ id:null, name:'', gender:'', age:''});
+};
+
+const handleDelete =(id)=>{
+setData(data.filter(item=>item.id !==id));
+};
+const handleEdit =(item)=>{
+    setIsEditing(item.id);
+    setFormData(item);
+};
+const handleUpdate =(id)=>{
+    setData(data.map(item=>(item.id ===id ? formData:item)));
+    setIsEditing(null);
+    etFormData({ id: null, name: '', gender: '', age: '' });
+}
+
+
+
+}
